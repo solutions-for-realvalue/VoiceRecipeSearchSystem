654654e71f59e219772cb6338009737214ea01c4
Arnaldo Sepulveda <natureuplift@protonmail.com>
2025-02-10 01:58:06 -0400

    Update relevantrecipe.js

--- script/relevantrecipe.js
+++ script/relevantrecipe.js
@@ -4,5 +4,16 @@ let idSearchResponse;
 
 function recipeInfoFetch(recipeSelected) {
 
+  // to hide recipe cards
+  $("#recipeResultsList").empty();
+
+  // show recipe id
+  console.log(recipeSelected);  // TODO comment when no longer needed
+  //insert id from local storage
+  let queryId = recipeSelected;
+  let AUTH = "apiKey=9cf7f51e45f542c98bb4360739b56ced";
+
+  const requestUrl = "https://api.spoonacular.com/recipes/" + queryId + "/information?" + AUTH;
+
   
 }
