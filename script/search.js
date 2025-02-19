903371b74ed29bb1bb37c15c23ee8907cf156687
Arnaldo Sepulveda <natureuplift@protonmail.com>
2025-02-19 01:32:36 -0400

    Create search.js

---
+++ script/search.js
@@ -0,0 +1,5 @@
+// userRecipe = "lasagna"; // TODO uncomment when want to use user recipe request from audio
+let recipeSearchResponse;  // intitialize global variable for searching recipe options
+// searchRecipeOptions('GetRecipe','chicken soup noodles'); // use when testing without user input
+
+
