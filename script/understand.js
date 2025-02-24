53b5b03434c31e8c2797ac3ef2d1aec8d9f53460
Arnaldo Sepulveda <natureuplift@protonmail.com>
2025-02-24 02:00:26 -0400

    Update understand.js

--- script/understand.js
+++ script/understand.js
@@ -17,3 +17,29 @@ $("#search-btn").click(function () {
     }
 });
 
+// Use Wit.ai which is a natural language processing (NLP) platform
+// - used for building applications that can understand and interpret human language
+//  Goal is to train wit.ai applications to understand the intent behind user input
+
+function getUserRecipe(userRecipeRequest) {
+
+    console.log("started using Wit function") // TODO to comment when functions working
+    
+    //  $ curl \'https://api.wit.ai/message?v=20230215&q=hello' \
+    //  -H "Authorization: Bearer $TOKEN"
+    // Wit.ai API endpoint
+
+    const question = encodeURIComponent(userRecipeRequest);
+    const uri = 'https://api.wit.ai/message?v=20231202&q=' + question;
+    const auth = 'Bearer ' + 'V3EZPKQJCUMULEEUOE3WLKPSUMO7HMSP'; // TODO hide auth before pushing to git
+
+    fetch(uri, {headers: {Authorization: auth}})
+        .then(function(response) {
+            return response.json();
+        })
+    
+    
+}
+
+// if the translation intent is detected via --> search.js
+// otherwise request user to clarify/change the recipe question
